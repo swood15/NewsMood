f17e77f726fcfa51677b9d5b7e4ad7c6737ed8b9
Sam Wood <41810698+swood15@users.noreply.github.com>
2018-10-03 23:21:25 -0700

    changes

---
+++ config.py
@@ -0,0 +1,9 @@
+# Add your API key
+owm_key = "296377f1e549da791b8add68d55cade0"
+nyt_key = "a8c29774af88442db2d5c5343b297f39"
+gmap_key = "AIzaSyCxHHJZCNFwo1FQupLY6mknIM3fKAv14PU"
+cens_key = "85ac64b6b5a9c0901b00329d1ef41f0c53ccfc98"
+consumer_key = "xblnIBcMUpb23CnqoPMR70Soi"
+consumer_secret = "1pZfqXZivtm9qCvcklT8moVMefK1qTsXlwk8XarbEPBZA6sUjB" 
+access_token = "1039732461784260609-qUT0cMTHYoV36YIXJlKBTKiAcR0rVX"
+access_token_secret = "KZLJ6kUWMwwdCVEbo7aVycUPHSQZcX7ITjWOkb9NNkn8o"
